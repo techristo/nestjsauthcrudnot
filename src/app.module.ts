95c8ca773eaea26678d7140d0ca05172c4a8a95f
TECHRISTO <49098166+techristo@users.noreply.github.com>
2022-02-05 15:13:12 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,11 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { MailModule } from './mail/mail.module';
-import { QuotesService } from './services/quotes/quotes.service';
+
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'),ConfigModule.forRoot({
-    isGlobal: true, // no need to import into other modules
-  }), AuthModule, UsersModule, ProductsModule, MailModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'),AuthModule, UsersModule, ProductsModule, MailModule],
   controllers: [AppController],
-  providers: [AppService, QuotesService],
+  providers: [AppService],
 })
 export class AppModule {}
